3a5e19ecf549db19d2016376d9278d374aca1fe2
Hakon Verespej <hverespej@arivale.com>
2017-05-07 03:04:08 -0700

    Add some initial content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,56 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Baby Books',
+    ages: ['< 2', '2', '3', '4', '5+'],
+    genders: [
+      {
+        image: '/images/gender-boy.jpg',
+        text: 'Boy',
+      },
+      {
+        image: '/images/gender-girl.jpg',
+        text: 'Girl',
+      },
+      {
+        image: '/images/gender-neutral.jpg',
+        text: 'Neutral',
+      },
+    ],
+    locations: [
+      {
+        image: '/images/location-city.jpg',
+        text: 'City',
+      },
+      {
+        image: '/images/location-farm.jpg',
+        text: 'Farm',
+      },
+      {
+        image: '/images/location-suburb.jpg',
+        text: 'Suburb',
+      },
+    ],
+    interests: [
+      {
+        image: '/images/interest-animals.jpg',
+        text: 'Animals',
+      },
+      {
+        image: '/images/interest-cars.jpg',
+        text: 'Cars',
+      },
+      {
+        image: '/images/interest-princesses.jpg',
+        text: 'Princesses',
+      },
+      {
+        image: '/images/interest-superheroes.jpg',
+        text: 'Superheroes',
+      },
+    ],
+  });
 });
 
 module.exports = router;
